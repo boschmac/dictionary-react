c7dbf71eefe757d416cba6505f97f1a3ba4c1365
boschmac <90581512+boschmac@users.noreply.github.com>
2022-04-10 20:10:23 -0400

    Setting up word search for dictionary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
+        Dictionary
       </header>
+      <main> 
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Corinne Long</small>
+      </footer>
+    </div>
     </div>
   );
 }
