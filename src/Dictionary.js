1ef9fe0ae9bbb66939ada86f1ed3f0fbd049a789
boschmac <90581512+boschmac@users.noreply.github.com>
2022-04-10 20:17:11 -0400

    fixed bug

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,6 +5,10 @@ import "./Dictionary.css";
 export default function Dictionary (){
 let [keyword, setKeyword] = useState("");
 
+function handleResponse (response){
+  console.log(response.data[0]);
+}
+
 function handleKeywordChange(event){
   setKeyword(event.target.value);
 }
@@ -14,6 +18,7 @@ event.preventDefault();
 alert(`Searching for ${keyword}`);
 
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+axios.get(apiUrl).then(handleResponse);
   }
   
   return (
