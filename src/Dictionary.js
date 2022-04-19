f489316ef81f50bceda00de2c57b231ba2aaafc9
boschmac <90581512+boschmac@users.noreply.github.com>
2022-04-18 21:15:51 -0400

    Image search added to bottom of page

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary (){
 let [keyword, setKeyword] = useState("");
 let [results, setResults] = useState (null);
+let [photo, setPhoto] = useState(null);
 
-function handleResponse (response){
+function handleDictionaryResponse (response){
 
 setResults (response.data[0]);
 }
 
+function handlePexelResponse (response){
+  setPhoto(response.data.photos);
+}
 function handleKeywordChange(event){
   setKeyword(event.target.value);
 }
@@ -21,7 +26,13 @@ event.preventDefault();
 
 
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
-axios.get(apiUrl).then(handleResponse);
+axios.get(apiUrl).then(handleDictionaryResponse);
+
+const pexelApiKey= "563492ad6f9170000100000135f282540fe042758c498124d30319a5";
+let pexelApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+let headers =  { Authorization: `Bearer ${pexelApiKey}` };
+
+axios.get(pexelApiUrl, {headers: headers }).then(handlePexelResponse);
   }
   
   return (
@@ -32,6 +43,7 @@ axios.get(apiUrl).then(handleResponse);
     </form>
     </section>
     <Results results={results}/>
+    <Photos photos={photo}/>
   </div>
   )
 }
